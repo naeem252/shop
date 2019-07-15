1b666a7b1b5dd238f5e582599f78dd8cf3404346
naeem hasan <naeemhasan252@gmail.com>
2019-07-15 15:10:40 +0600

    add dropdown

--- js/main.js
+++ js/main.js
@@ -8,15 +8,43 @@ $(this).addClass('active');
 
     $('.top_nav_link').click(function(){
 
-        $('.top_nav_link').css("opacity","1");
-        $(this).css("opacity",".5");
-            
+        $('.top_nav_link').removeClass('active_t');
+        // $(this).toggleClass("active_t");
+        if(!$(this).hasClass('active_t')){
+            $(this).addClass('active_t');
+        }
+        
+        else if($(this).hasClass('active_t')){
+            console.log(5555);
+        }
     });//for top nav
+
+    // user sub
+//    $('.user_sub').hide();
+var slide=false;
+    $('#user').click(function(){
+        if(!slide){
+            $('.user_sub').css('transform',"scaleY(1)");
+            // setTimeout(()=>$('.user_sub').css('transform',"translateY(10px)"),300);
+            slide=true;
+        }else if(slide){
+            // $('.user_sub').css('transform',"translateY(0px)");
+            // setTimeout(()=>,300);
+            $('.user_sub').css('transform',"scaleY(0)")
+            
+         
+            slide=false;
+        }
+
+
+    });
 
 $(document).click(function(e){
     if(!$(e.target).is('.top_nav_item,.top_nav_link,.top_nav_link i,.side_nav_link')){
-        $('.top_nav_link').css("opacity","1");
+        $('.top_nav_link').removeClass('active_t');
         $('.side_nav_link').removeClass('active');
+        $('.user_sub').css("transform","scaleY(0)");
+        slide=false;
     }
 });//document after click
     
